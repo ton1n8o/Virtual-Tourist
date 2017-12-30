afc9d9a3b60abdd7f510e862fb3c92111aef3744
Antonio Carlos <antoniocarlos.dev@gmail.com>
2017-12-30 15:16:20 -0200

    Photo fetch and save refactoring

--- Virtual Tourist/ViewControllers/PhotoAlbumViewController.swift
+++ Virtual Tourist/ViewControllers/PhotoAlbumViewController.swift
@@ -20,7 +20,6 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
     // MARK: - Variables
     
     var pin: Pin?
-    var ctx: NSManagedObjectContext?
     
     // MARK: - UIViewController lifecycle
     
@@ -30,75 +29,17 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
         mapView.isZoomEnabled = false
         mapView.isScrollEnabled = false
         
-        ctx = coreDataStack.context
-        
         guard let pin = pin else {
             return
         }
         showOnTheMap(pin)
-        
-        let lat = Double(pin.latitude!)!
-        let lon = Double(pin.longitude!)!
         
         if let photos = loadPhotos(using: pin), !photos.isEmpty {
             print("\(#function) photos \(photos.count)")
         } else {
             print("\(#function) no photos")
             // pin selected has no photos
-            
-            func showErrorMessage(msg: String) {
-                self.showInfo(withTitle: "Error", withMessage: msg)
-            }
-            
-            Client.shared().searchBy(latitude: lat, longitude: lon) { (photosParsed, error) in
-                if let photosParsed = photosParsed {
-                    
-                    print("\(#function) Downloading \(photosParsed.photos.photo.count) photos.")
-                    
-                    let errorMessage = " image(s) could not be downloaded."
-                    var errorCount = 0
-                    
-                    for (idx, photo) in photosParsed.photos.photo.enumerated() {
-                        
-                        Client.shared().downloadImage(imageUrl: photo.url) { (data, error) in
-                            
-                            if let data = data {
-                                
-                                print("\(#function) Downloading \(idx)")
-                                
-                                self.performUIUpdatesOnMain {
-                                    let photoCD = Photo(photoData: data, forPin: pin, context: self.ctx!)
-                                    photoCD.pin = pin
-                                }
-                                
-                            } else if let _ = error {
-                                errorCount += 1
-                            }
-                            
-                            if idx == photosParsed.photos.photo.count - 1 {
-                                
-                                print("\(#function) DONE")
-                                
-                                self.performUIUpdatesOnMain {
-                                    self.save()
-                                }
-                                
-                                if errorCount > 0 {
-                                    let message = "\(errorCount) " + errorMessage
-                                    showErrorMessage(msg: message)
-                                }
-                            }
-                            
-                        }
-                        
-                    }
-                    
-                } else if let error = error {
-                    print("\(#function) error:\(error)")
-                    self.showInfo(withTitle: "Error", withMessage: "Error while fetching Photos: \(error)")
-                }
-            }
-            
+            fetchPhotosFromAPI(pin)
         }
     }
     
@@ -109,6 +50,63 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
     }
     
     // MARK: - Helpers
+    
+    private func fetchPhotosFromAPI(_ pin: Pin) {
+        
+        let lat = Double(pin.latitude!)!
+        let lon = Double(pin.longitude!)!
+        
+        Client.shared().searchBy(latitude: lat, longitude: lon) { (photosParsed, error) in
+            if let photosParsed = photosParsed {
+                print("\(#function) Downloading \(photosParsed.photos.photo.count) photos.")
+                self.storePhotos(photosParsed.photos.photo, forPin: pin)
+            } else if let error = error {
+                print("\(#function) error:\(error)")
+                self.showInfo(withTitle: "Error", withMessage: "Error while fetching Photos: \(error)")
+            }
+        }
+    }
+    
+    func storePhotos(_ photos: [PhotoParser], forPin: Pin) {
+        
+        func showErrorMessage(msg: String) {
+            self.showInfo(withTitle: "Error", withMessage: msg)
+        }
+        
+        let errorMessage = " image(s) could not be downloaded."
+        var errorCount = 0
+        
+        for (idx, photo) in photos.enumerated() {
+            
+            Client.shared().downloadImage(imageUrl: photo.url) { (data, error) in
+                
+                if let data = data {
+                    print("\(#function) Downloading \(idx)")
+                    self.performUIUpdatesOnMain {
+                        _ = Photo(photoData: data, forPin: forPin, context: self.coreDataStack.context)
+                    }
+                } else if let _ = error {
+                    errorCount += 1
+                }
+                
+                if idx == photos.count - 1 {
+                    
+                    print("\(#function) DONE")
+                    
+                    self.performUIUpdatesOnMain {
+                        self.save()
+                    }
+                    
+                    if errorCount > 0 {
+                        let message = "\(errorCount) " + errorMessage
+                        showErrorMessage(msg: message)
+                    }
+                }
+                
+            }
+            
+        }
+    }
     
     private func showOnTheMap(_ pin: Pin) {
         
